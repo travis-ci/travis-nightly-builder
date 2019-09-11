9b6450be221ecd51ffc259209a410c39d100f943
Hiro Asari <asari.ruby@gmail.com>
2019-09-11 07:56:51 -0400

    Add endpoint to display language archives
    Remove creds

--- config.ru
+++ config.ru
@@ -3,4 +3,6 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require 'travis'
 
+use Rack::Static, urls: ['/js', '/css', '/favicon.ico', '/img'], root: 'public'
+
 run Travis::NightlyBuilder::App
--- lib/travis/nightly_builder/app.rb
+++ lib/travis/nightly_builder/app.rb
@@ -2,6 +2,8 @@ require 'json'
 require 'sinatra/base'
 require 'sinatra/param'
 require 'sinatra/contrib'
+require 'google/cloud/storage'
+require 'ostruct'
 
 require_relative 'runner'
 
@@ -32,8 +34,19 @@ module Travis
 
       helpers Sinatra::Param
 
+      attr_reader :archives
+
       get '/hello' do
         "ohai\n"
+      end
+
+      get /\/builds(?:\/([^\/]+)(?:\/([^\/]+)(?:\/([^\/]+)(?:\/(.*))?)?)?)?/ do
+        # /builds/:lang
+        # /builds/:lang/:os
+        # /builds/:lang/:os/:release
+        # /builds/:lang/:os/:release/:arch
+        @archives = files(params['captures'])
+        slim :index
       end
 
       post '/build/:repo' do
@@ -41,11 +54,14 @@ module Travis
         param :env, Array, default: []
         param :source, String, default: ENV['DYNO']
 
+        form_data = JSON.parse request.body.read
+
         results = runner.run(
           repo: params['repo'],
           branch: params['branch'],
           env: params['env'],
-          source: params['source']
+          source: params['source'],
+          override: form_data
         )
 
         halt 400 unless results.success?
@@ -64,6 +80,35 @@ module Travis
           token: ENV.fetch('TRAVIS_TOKEN')
         )
       end
+
+      def gcs_viewer
+        @viewer ||= Google::Cloud::Storage.new(
+          project_id: ENV.fetch('TRAVIS_GCS_PROJECT_ID'),
+          credentials: gcs_read_creds
+        )
+      end
+
+      def gcs_read_creds
+        JSON.load(ENV.fetch('TRAVIS_GCS_CRED_JSON'))
+      end
+
+      def files(parts)
+        prefix = if parts.length > 1
+          parts.insert(1, 'binaries').compact.join("/")
+        else
+          parts.first
+        end
+
+        gcs_viewer.bucket('travis-ci-language-archives')
+        .files(prefix: prefix)
+        .all
+        .select {|x| x.name.end_with?('.bz2')}
+        .map do |x|
+          lang, _, os, release, arch, file_name = x.name.split('/')
+          OpenStruct.new(lang: lang, os: os, release: release, arch: arch, name: file_name)
+        end
+      end
+
     end
   end
 end
